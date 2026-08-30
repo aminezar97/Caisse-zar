3404f0f702ee034c1ed1c73730e1c5b91195f3f5
aminezar97 <your-email@example.com>
2026-08-30 16:28:52 +0100

    Force add android folder and web assets

---
+++ android/app/src/main/assets/public/vendor/README.txt
@@ -0,0 +1,1 @@
+html5-qrcode.min.js is copied here automatically by npm run prepare.
---
+++ android/app/src/main/java/com/zarouali/caisse/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zarouali.caisse;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
